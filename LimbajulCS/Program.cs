2d850f04ee26f597287af1f852a6a1bfbac229c5
Veres Roland <roli25veres@gmail.com>
2018-12-14 17:01:41 +0200

    Add project files.

---
+++ LimbajulCS/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace LimbajulCS
+{
+    class Program
+    {
+        //Functia main- versiuni
+        /*
+         * static void Main(string[] args){}
+         * static void Main(){}
+         *static  int Main(){return 0;}
+         * static int Main(string[] args){return 0;}
+         * */
+        static void Main(string[] args)
+        {
+            Console.ReadKey(); // Citeste un caracter pe care nu-l afiseaza
+                               //util  pentru a face programul sa nu iasa asa dintr-o data
+            #region regiune
+            Console.ReadKey();
+            #endregion
+        }
+    }
+}
